69422659048c0b01bff1685f94bc5a917da66dd1
Nikita Moshenskiy <nikita@moshensky.com>
2026-01-31 13:26:42 +0200

    WIP

--- examples/react-vite-ts/.storybook/preview.ts
+++ examples/react-vite-ts/.storybook/preview.ts
@@ -1,11 +1,12 @@
 import type { Preview } from '@storybook/react-vite'
+import 'loki/configure-react'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
